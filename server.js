2307a6f65837c8cddf12b128f789ea27cc6909ba
vingenesi <vinnyvvv04@gmail.com>
2022-11-16 13:00:17 -0300

    conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-var a = 'hello world'
+const express = require("express");
+const sequelize = require("./src/config/configDataBase");
+const app = express();
+
+const port = 3000
+
+app.get("/", function(req, res){
+    res.send("tarara")
+})
+
+app.listen(
+  port,
+  console.table({
+    ports: port,
+    DB: sequelize.config.host,
+  })
+);
