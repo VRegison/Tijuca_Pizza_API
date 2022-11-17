bac98086cc23e932134638d01f83941c02c906c1
ThayssaKetlen <est.thayssa.889@gmail.com>
2022-11-17 13:01:59 -0300

    puxando alteracoes

--- index.js
+++ index.js
@@ -4,30 +4,30 @@ const app = express()
 const port = 8080
 
 //PEGAR TODOS OS REGISTROS
-app.get("/", (req,res) => {
-    res.json({ mensagem:'pegar todos os registros!!'})
+app.get("/", (req, res) => {
+    res.json({ mensagem: 'pegar todos os registros!!' })
 })
 //PEGAR SOMENTE O REGISTRO PELO ID
-app.get("/:id", (req,res) => {
+app.get("/:id", (req, res) => {
     const id = req.params.id
-    res.json({ mensagem:`pegar somente o registro com id: ${id}`})
+    res.json({ mensagem: `pegar somente o registro com id: ${id}` })
 })
 //CRIAR UM REGISTRO
-app.post("/",(req,res) => {
+app.post("/", (req, res) => {
     const body = req.body
-    res.json(body)
+    res.send(body)
 })
 //ATUALIZAR SOMENTE O REGISTRO PELO ID
-app.put("/:id",(req,res) => {
+app.put("/:id", (req, res) => {
     const id = req.params.id
-    res.json({ mensagem:`atualizar somente o registro com id: ${id}`})
+    res.json({ mensagem: `atualizar somente o registro com id: ${id}` })
 })
 //DELETAR SOMENTE O REGISTRO PELO ID
-app.delete("/:id",(req,res) =>{
+app.delete("/:id", (req, res) => {
     const id = req.params.id
-    res.json({ mensagem:`deletar somente o registro com id: ${id}`})
+    res.json({ mensagem: `deletar somente o registro com id: ${id}` })
 })
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log('Servidor rodando !!!!')
 })
