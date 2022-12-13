4d7f4fe1b2a3d47d65a596b68dad8e439a628ed6
vingenesi <vinnyvvv04@gmail.com>
2022-12-13 13:07:09 -0300

    atualizacao

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,35 +4,27 @@ const router = Router();
 const User = require("../config/configDataBase");
 const SECRET = require("dotenv").config();
 
-//user rotas
+//cadastro e login rotas
 const loginValidation = require("../../validations/login");
 const Login = require("../controllers/login");
 const cadValidation = require("../../validations/cad");
 const Create = require("../controllers/cadastro");
 
-//pedidos rotas
-const {
-  createPedido,
-  listarPedido,
-  updatePed,
-} = require("../controllers/pedidos");
-
-//produto
-const { createProduto } = require("../controllers/produtos");
-const { listarProduto } = require("../controllers/produtos");
-const { updateProduto } = require("../controllers/produtos");
-const verifyToken = require("../../validations/token.validation");
-
-//User
 router.post("/login", loginValidation, Login);
 router.post("/create", cadValidation, Create);
 
-//Produtos
-router.post("/createProdutos", createProduto);
-router.get("/listarProdutos", verifyToken, listarProduto);
-router.patch("/updateProdutos/:id", updateProduto);
+//Rotas produtos
+const { createProduto, listarProduto, updateProduto } = require("../controllers/produtos");
+const verifyToken = require("../../validations/token.validation");
 
-//Pedidos rotas
+router.post("/createProdutos", verifyToken, createProduto);
+router.get("/listarProdutos", verifyToken, listarProduto);
+router.patch("/updateProdutos/:id", verifyToken, updateProduto);
+
+
+//Rotas pedidos
+const { createPedido, listarPedido, updatePed } = require("../controllers/pedidos");
+
 router.post("/createPedidos", createPedido);
 router.get("/listarPedidos", listarPedido);
 router.patch("/updatePedidos/:id", updatePed);
